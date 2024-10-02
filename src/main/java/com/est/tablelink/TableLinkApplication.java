65f736dae1853a6ecb091018fb17be29b7f71b70
jojunho <junhosvain@naver.com>
2024-10-02 14:20:11 +0900

    :rocket: 프로젝트 시작

---
+++ src/main/java/com/est/tablelink/TableLinkApplication.java
@@ -0,0 +1,13 @@
+package com.est.tablelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/est/tablelink/TableLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.tablelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
