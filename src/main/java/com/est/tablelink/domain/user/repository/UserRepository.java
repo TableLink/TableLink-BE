e687a94491437a65b05ab1c2427def254918a6d7
jojunho <junhosvain@naver.com>
2024-10-06 14:50:39 +0900

    Feat: User 관련 기본 클래스 생성

---
+++ src/main/java/com/est/tablelink/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.est.tablelink.domain.user.repository;
+
+import com.est.tablelink.domain.user.domain.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
