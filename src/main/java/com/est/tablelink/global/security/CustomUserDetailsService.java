6e0f9410557adf30028c17c98ea20c67ee1900e5
jojunho <junhosvain@naver.com>
2024-10-09 17:20:40 +0900

    Feat: Security 로직 수정 및 PasswordEncoder 추가

--- src/main/java/com/est/tablelink/global/security/CustomUserDetailsService.java
+++ src/main/java/com/est/tablelink/global/security/CustomUserDetailsService.java
@@ -1,7 +1,11 @@
 package com.est.tablelink.global.security;
 
+import com.est.tablelink.domain.user.domain.User;
+import com.est.tablelink.domain.user.dto.response.UserResponseDto;
 import com.est.tablelink.domain.user.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,7 +19,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return (UserDetails) userRepository.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return new CustomUserDetails(UserResponseDto.toDto(user));
     }
 }
--- src/main/java/com/est/tablelink/global/security/SecurityConfig.java
+++ src/main/java/com/est/tablelink/global/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.est.tablelink.global.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +19,7 @@ public class SecurityConfig {
 
     private final JwtRequestFilter jwtRequestFilter;
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -23,7 +27,7 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable);
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/user/signup").permitAll()
+                        .requestMatchers("/api/user/signup", "/api/user/signin").permitAll()
                         .anyRequest().authenticated());
         http
                 .sessionManagement(
@@ -31,5 +35,11 @@ public class SecurityConfig {
         http
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
