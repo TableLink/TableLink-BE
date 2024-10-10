51b1be924856787a438ac935283d5c549ad25979
jojunho <junhosvain@naver.com>
2024-10-10 13:56:59 +0900

    Feat: Security 변수명 변경

--- src/main/java/com/est/tablelink/global/security/CustomUserDetails.java
+++ src/main/java/com/est/tablelink/global/security/CustomUserDetails.java
@@ -1,19 +1,18 @@
 package com.est.tablelink.global.security;
 
-import com.est.tablelink.domain.user.dto.response.UserResponseDto;
+import com.est.tablelink.domain.user.dto.response.UserResponse;
 import java.util.Collection;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
 @Getter
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
-    private final UserResponseDto userResponseDto;
+    private final UserResponse userResponseDto;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
