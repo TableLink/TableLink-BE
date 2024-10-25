7c8cc47f1c98fcd08e88fb5310505cb7abb59ccb
jojunho <junhosvain@naver.com>
2024-10-25 18:08:33 +0900

    Feat: 게시글 DTO, Repository, Controller, Service 생성

---
+++ src/main/java/com/est/tablelink/domain/post/dto/response/DetailPostResponse.java
@@ -0,0 +1,29 @@
+package com.est.tablelink.domain.post.dto.response;
+
+import com.est.tablelink.domain.post.domain.Content;
+import com.est.tablelink.domain.post.domain.Post;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class DetailPostResponse {
+    private Long id; // 게시글 id
+    private String title; // 게시글 제목
+    private String author; // 게시글 작성자 닉네임
+    private Content content; // 게시글 내용
+    private LocalDateTime createdAt; // 게시글 생성 일시
+
+    public DetailPostResponse toDto(Post post){
+        return DetailPostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .author(post.getAuthor().getNickname())
+                .content(post.getContent())
+                .createdAt(post.getCreatedAt())
+                .build();
+    }
+}
---
+++ src/main/java/com/est/tablelink/domain/post/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.est.tablelink.domain.post.repository;
+
+import com.est.tablelink.domain.post.domain.Post;
+import com.est.tablelink.domain.user.domain.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    // 게시글 작성자 찾기
+    List<Post> findAllByAuthor(User user);
+}
